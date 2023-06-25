c3f7c6054e1a309a2ef40f6abbc8c97b79c00587
KEVIN <402308383@qq.com>
2023-06-25 11:12:55 +0800

    提交文件

---
+++ project_c/6-23/6-23/test1.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+int main()
+{
+	//if不能循环，判断为真则只执行一次，而while可
+	//getchar()和putchar（)接收的是一个字符而不是字符串，getchar（）的返回值是ASCII码，属于整型。
+	int ret = 0;
+	char password[20] = { 0 };
+	printf("INPUTHA:\n");
+	scanf("%s", password);
+	printf("please（Y/N）");
+	ret = getchar();
+	if (ret == 'Y')
+		printf("yes\n");
+	else
+		printf("no\n");
+	return 0;
+}
---
+++ project_c/6_13/zz/zz/zz.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int a=20000, i=0;
+	while (i < a)
+	{
+		printf("%d\n", i);
+		i += 1;
+	}
+	return 0;
+
+}
