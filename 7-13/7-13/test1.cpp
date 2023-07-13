85a1c77bba65dc3226efb7fe44cf00507db2d4c0
402308383 <137171780+402308383@users.noreply.github.com>
2023-07-13 22:35:36 +0800

    代码提交

---
+++ 7-13/7-13/test1.cpp
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a = 0, b = 0, c = 0;
+
+
+	
+	return 0;
+}
+
+
+
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int max = arr[0];
+//	int i = 0;
+//	int sz = sizeof(arr) /sizeof(arr[0]);
+//	for (i = 0; i < sz; i += 1)
+//	{
+//		if (arr[i] >= max)
+//			max = arr[i];
+//	}
+//	printf("最大值为%d\n",max);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	float sum = 0.0;
+//	int flag = 1;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		sum += flag*1.0/ i;
+//		flag = -flag;
+//	}
+//	printf("%f\n", sum);
+//	return 0;
+//}
