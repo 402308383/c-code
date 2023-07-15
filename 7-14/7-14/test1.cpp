ffcf2dd3b4e608fc9a3093bc6a524c386b0a5c0a
402308383 <137171780+402308383@users.noreply.github.com>
2023-07-15 16:31:17 +0800

    代码提交

--- 7-14/7-14/test1.cpp
+++ 7-14/7-14/test1.cpp
@@ -1,27 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-//猜数字游戏
-//1.电脑生成一个随机数2.猜数字
-void menu()
-{
-	printf("*****************************\n");
-	printf("******* 1.play 0.exit *******\n");
-	printf("*****************************\n");
-}
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
 
-//时间戳
+//goto可跳出多层嵌套
+//shutdown -s -t 60关机指令 shutdown -a 取消关机
+//system()-执行系统命令
 int main()
 {
-	int input = 0;
-
-
-	do
+	char input[20] = {};
+	system("shutdown -s -t 60");
+	while (1)
 	{
-		menu();
-		printf("请选择>:");
-	}while ();
-	
-	return 0;
+		printf("请注意，您的电脑将在60s内关机，您可以输入：“我是猪”来取消关机\n");
+		scanf("%s", input);
+		if (strcmp(input, "我是猪") == 0)
+		{
+			system("shutdown -a");
+			printf("真听话，已取消关机\n");
+			break;
+		}
+	}
+		return 0;
 }
 
 
@@ -31,10 +32,59 @@ int main()
 
 
 
-
-
-
-
+//猜数字游戏
+//1.电脑生成一个随机数2.猜数字
+//void menu()
+//{
+//	printf("***************************\n");
+//	printf("****** 1.play 0.exit ******\n");
+//	printf("***************************\n");
+//}
+//void game()
+//{
+//	int i = rand() % 100+1, a = 0;
+//	while (1)
+//	{
+//		printf("请猜数字:\n");
+//		scanf("%d", &a);
+//		if (i > a)
+//			printf("猜小了\n");
+//		else if (i < a)
+//			printf("猜大了\n");
+//		else
+//		{
+//			printf("猜对了");
+//			break;
+//		}
+//	}
+//
+//}
+////时间戳
+//int main()
+//{
+//	int input = 0;
+//	srand((unsigned int) time(NULL));//生成随机数起点,最好不要放在循环体内部，一个主函数放一个就好
+//	do
+//	{
+//		menu();
+//		printf("请选择>:");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			game();		
+//			break; 
+//		case 0:
+//			printf("退出游戏");
+//			break;
+//		default:
+//			printf("输入错误，请重新输入\n");
+//			break;
+//		}
+//	}while (input);
+//	
+//	return 0;
+//}
 
 //99乘法口诀表
 //int main()
