2f669525d64edeadd70a2cedb537a7338017b6fe
402308383 <402308383@qq.com>
2023-10-07 23:22:54 +0800

    代码提交

---
+++ 10-7/Project1/test.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+//int main()
+//{
+//	/*int b, a = 12, n = 5;
+//	printf("%d", a %=(b = 2, n %= b) + 4);*/
+//	int i = 10, j = 5, x, y;
+//	printf("%d\n", i++ - ++j);
+//	printf("i=%d,j=%d\n", i, j);
+//	x = i++;
+//	y = ++i * j--;
+//	printf("x=%d,y=%d\n", x, y);
+//
+//	return 0;
+//}
