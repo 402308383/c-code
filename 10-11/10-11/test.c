afb111608b442ce726bd167256ffd09083ad89c3
402308383 <402308383@qq.com>
2023-10-11 23:51:54 +0800

    代码提交

---
+++ 10-11/10-11/test.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+//	int  i, j, k = 0;
+//	for (i = 1; i <= 3; i++)
+//		for (j = 1; j <= i; j++)
+//			k += i * j;
+//	printf("%d", k);
+	/*for (a = 0; a++ <= 2;)
+	{
+		;
+	}
+	b += a;
+	printf("%d,%d\n", a, b);
+	return 0;*/
+}
