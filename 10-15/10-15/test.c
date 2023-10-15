5b5fda3f5ef213eceab3dd4f3ece79ce78a99f2b
402308383 <402308383@qq.com>
2023-10-16 00:03:27 +0800

    代码提交

---
+++ 10-15/10-15/test.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	//float a = 9.5;
+	int a = 1;
+	int b = 2;
+	getchar();
+	{
+		int a = 5;
+		int b = 10;
+		getchar();
+
+	}
+	return 0;
+}
